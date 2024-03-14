698fec859b9e694e4223b27b3d9ac3349b36f075
Mok Yobin <dyqls1214@naver.com>
2024-03-15 08:53:21 +0900

    update : BOJ_3190_뱀.java

--- 목요빈/0315/BOJ_3190_뱀.java
+++ 목요빈/0315/BOJ_3190_뱀.java
@@ -1,103 +1,101 @@
-package self.march._0313;
-
 import java.io.*;
 import java.util.*;
 
 public class BOJ_3190_뱀 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int K = Integer.parseInt(br.readLine());
+	static int N, K, L;
+	static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
+	static class Snake{
+		int hr, hc, d;
+		Queue<Pos> que = new LinkedList<>();
 
-        int[][] map = new int[N+1][N+1];
+		public Snake(int hr, int hc, int d) {
+			super();
+			que.add(new Pos(hr, hc));
+			this.d = d;
+		}
+	}
+	
+	static class Pos{
+		int r, c;
 
-        //사과가 있는 위치는 1로 초기화
-        for(int i = 0; i<K; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+		public Pos(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
 
-            int row = Integer.parseInt(st.nextToken());
-            int col = Integer.parseInt(st.nextToken());
-
-            map[row][col] = 1;
-        }
-
-        int L = Integer.parseInt(br.readLine());
-
-        //시간에 따라서 회전시킬 방향 queue 생성
-        Queue<spin> spin = new LinkedList<>();
-        for(int i = 0; i<L; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int time = Integer.parseInt(st.nextToken());
-            String dir = st.nextToken();
-
-            spin.offer(new spin(time, dir));
-        }
-
-        //0은 북, 1은 동, 2는 남, 3은 서 의미
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1};
-
-        //초기 (1, 1)에서 시작하고 동쪽을 보고 시작
-        int row = 1;
-        int col = 1;
-        int time = 0;
-        int dir = 1;
-        Queue<Node> q  = new LinkedList<>();
-        q.offer(new Node(row, col));
-        map[row][col] = 2;
-
-        while(true){
-            int dR = row + dr[dir];
-            int dC = col + dc[dir];
-
-            time++;
-
-            if(dR<1||dC<1||dR>N||dC>N)
-                break;
-            if(map[dR][dC] == 2)
-                break;
-
-            if(map[dR][dC] == 0){
-                Node node = q.poll();
-                map[node.row][node.col] = 0;
-            }
-            if(!spin.isEmpty()) {
-                if (time == spin.peek().time) {
-                    spin s = spin.poll();
-
-                    if (s.dir.equals("L"))
-                        dir = dir-1 < 0 ? 3 : dir-1;
-                    if (s.dir.equals("D"))
-                        dir = dir+1 > 3 ? 0 : dir+1;
-                }
-            }
-
-            map[dR][dC] = 2;
-            q.offer(new Node(dR, dC));
-            row = dR;
-            col = dC;
-        }
-
-        System.out.println(time);
-    }
-}
-class spin{
-    int time;
-    String dir;
-
-    spin(int time, String dir){
-        this.time = time;
-        this.dir = dir;
-    }
-}
-
-class Node{
-    int row, col;
-
-    Node(int row, int col){
-        this.row = row;
-        this.col = col;
-    }
+		@Override
+		public String toString() {
+			return "Pos [r=" + r + ", c=" + c + "]";
+		}
+		
+	}
+	
+	/*
+	 뱀 이동
+	 - 머리 이동
+	 - 머리가 위치한 칸에 사과가 있으면 사과 먹기
+	 - 사과가 없으면 꼬리 한 칸 줄이기
+	 * */
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine()); // 사과의 개수 <= 100
+		
+		int[][] board = new int[N][N];
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken())-1;
+			board[r][c] = 1;
+		}
+		
+		L = Integer.parseInt(br.readLine()); // 방향 변환 정보 <= 100
+		int[][] rotate = new int[L][2];
+		for (int i = 0; i < L; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			char c = st.nextToken().charAt(0);
+			rotate[i][0] = r; // 시간
+			rotate[i][1] = c=='D' ? 1 : 0; // 1이면 오른쪽, 0이면 왼쪽 회전
+		}
+		
+		int time = 1;
+		int idx = 0;
+		Snake s = new Snake(0, 0, 1);
+		board[0][0] = 2;
+		// 뱀: 2, 사과: 1
+		while(true) {
+			// 뱀 이동
+			int nr = s.hr + dr[s.d];
+			int nc = s.hc + dc[s.d];
+			if(nr < 0 || nr >= N || nc < 0 || nc >= N || board[nr][nc] == 2) break;
+			
+			if(board[nr][nc] == 0) {
+				Pos p = s.que.poll(); // 사과를 먹지 못하면 꼬리 이동
+				board[p.r][p.c] = 0;
+			}
+			s.que.offer(new Pos(nr, nc)); // 머리 이동
+			s.hr = nr;
+			s.hc = nc;
+			board[nr][nc] = 2; // 뱀 이동 표시
+			
+			time += 1;
+			// 방향 전환
+			if(idx < L) {
+				if(time > rotate[idx][0]) {
+					if(rotate[idx][1] == 0) {
+						s.d = (s.d + 4 - 1) % 4;
+					}else {
+						s.d = (s.d + 1) % 4;
+					}
+					idx += 1;
+				}
+			}
+		}
+		System.out.println(time);
+	}
 }
