35a0812f94c6ca248a16b7f556912b57dcbec0f4
jiu <118248199+jiu6525@users.noreply.github.com>
2024-03-15 08:59:26 +0900

    Update 백준_17779_게리맨더링2.java

--- 강지우/백준_17779_게리맨더링2.java
+++ 강지우/백준_17779_게리맨더링2.java
@@ -1,126 +1,102 @@
-package jiu;
-
 import java.io.*;
 import java.util.*;
 
-//먼저 뱀은 몸길이를 늘려 머리를 다음칸에 위치시킨다.
-//만약 벽이나 자기자신의 몸과 부딪히면 게임이 끝난다.
-//만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다.
-//만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다. 즉, 몸길이는 변하지 않는다.
-//X초가 끝난 뒤에 왼쪽(C가 'L') 또는 오른쪽(C가 'D')로 90도
+// 모든 기준 위치에서 완탐을 해보자 d1,d2
+// 0,0 의 위치는 할 필요 없음
 public class Main {
-    static class direction{
-        int turn;
-        int d;
-
-        public direction(int turn, int d) {
-            this.turn = turn;
-            this.d = d;
-        }
-    }
     static BufferedReader br;
     static StringTokenizer st;
-    static int n,k,l,ans = 0;
-    static Queue<direction> snake_dir;
-    static int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
-    static LinkedList<int[]> snake,apple;
-    static int c = 0;
-    public static void main(String args[]) throws IOException {
+    static int n,mapSum,ans;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
-        k = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
 
-        snake = new LinkedList<>();
-        apple = new LinkedList<>();
-        snake_dir = new LinkedList<>();
-        snake.add(new int[]{0,0});
-
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken())-1;
-            int y = Integer.parseInt(st.nextToken())-1;
-            apple.add(new int[]{x,y});
-        }
-
-        l = Integer.parseInt(br.readLine());
-        for (int i = 0; i < l; i++) {
-            st = new StringTokenizer(br.readLine());
-            int turn = Integer.parseInt(st.nextToken());
-            char num = st.nextToken().charAt(0);
-            int d = num == 'L' ? -1 : 1;
-            snake_dir.add(new direction(turn,d));
-        }
-
-//          자기 몸에 닿는지 확인
-//          전진
-//          방향 전환 확인    turn 이 끝난뒤에 진행
-//          사과 먹으면 연결리스트 추가
-//          몸길이 즈악
-//          정답 누적
-        int dx = 0;
-        int dy = 0;
-        while(true){
-            ans++;
-//          이동
-            int nx = dx + dir[c][0];
-            int ny = dy + dir[c][1];
-
-//          범위 벗어나고 자기 몸이랑 부딪히면
-            if(!crush(nx, ny)){
-                break;
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                mapSum += arr[i][j];
             }
-
-//           전진 사과 확인 -> 몸길이 증가
-            snake.addFirst(new int[] { nx, ny });
-            if(!eatCheck(nx, ny, apple)){
-                snake.removeLast();
-            }
-
-//          방향 전환
-            changeDir();
-            dx = nx;
-            dy = ny;
         }
+
+        ans = Integer.MAX_VALUE;
+        for (int si = 0; si < n-2; si++) {
+            for (int sj = 1; sj < n-1; sj++) {      // 가능한 모든 기준위치  d1을 기준을 이동 후 d2 이동 -> 범위 내부에서만
+                for (int d1 = 1; d1 < n; d1++) {    // 범위내의 조건일때의 d1
+                    if(0 <= si + d1 && si + d1 < n && 0 <= sj - d1 && si - d1 < n ){
+                        for (int d2 = 1; d2 < n; d2++) {
+                            if(0 <= si + d1 + d2 && si + d1 + d2 < n && 0 <= sj + d2 && sj + d2 < n ){
+                                ans = Math.min(ans, cal(si,sj,d1,d2));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+//        print();
         System.out.println(ans);
     }
 
-    private static void changeDir() {
-        int size = snake_dir.size();
-        for (int i = 0; i < size; i++) {
-            direction dir_check = snake_dir.poll();
-            if(dir_check.turn == ans){
-                c += dir_check.d;
-                c = c < 0 ? dir.length-1 : (c == dir.length? 0 : c);
-                break;
+    private static int cal(int si, int sj, int d1, int d2) {
+        int[][] v = new int[n][n];
+        int[] lst = new int[5];
+
+        // 5번구역에 표시
+        v[si][sj] = 1;
+        int j1 = sj;
+        int j2 = sj;
+        for (int di = 1; di < d1+d2+1; di++) {
+            if(di<=d1){
+                j1--;
             }else{
-                snake_dir.add(dir_check);
+                j1++;
+            }
+
+            if(di<=d2){
+                j2++;
+            }else{
+                j2--;
+            }
+
+            for (int i = j1; i < j2+1; i++) {
+                v[si+di][i] = 1;
             }
         }
+
+        // 1~4구역의 인구수 누적
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(v[i][j] == 1){
+                    continue;
+                }
+                if(i<si+d1 && j<=sj){
+                    lst[0] += arr[i][j];
+                }
+                if(i<=si+d2 && sj<j){
+                    lst[1] += arr[i][j];
+                }
+                if(si+d1<=i && j<sj-d1+d2){
+                    lst[2] += arr[i][j];
+                }
+                if(si+d2<i && sj-d1+d2<=j){
+                    lst[3] += arr[i][j];
+                }
+            }
+        }
+
+        // 5구역 인구수
+        lst[4] = mapSum - lst[0] - lst[1] - lst[2] - lst[3];
+        int max = Arrays.stream(lst).max().getAsInt();
+        int min = Arrays.stream(lst).min().getAsInt();
+        return max-min;
     }
 
-    private static boolean eatCheck(int nx, int ny, LinkedList<int[]> arr) {
-        int size = arr.size();
-        for (int i = 0; i < size; i++) {
-            int[] p = arr.poll();
-            if (Arrays.equals(p, new int[]{nx, ny})) {
-                return true;
-            }else{
-                arr.offer(p);
-            }
+    private static void print() {
+        for (int[] lst : arr) {
+            System.out.println(Arrays.toString(lst));
         }
-        return false;
-    }
-
-    private static boolean crush(int nx, int ny) {
-        if(nx < 0 || nx >= n || ny < 0 || ny>=n){
-            return false;
-        }
-        for (int[] body : snake) {
-            if (Arrays.equals(body, new int[]{nx, ny})) {
-                return false;
-            }
-        }
-        return true;
     }
 }
